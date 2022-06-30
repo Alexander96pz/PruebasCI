90d22f85822225169708a1864f095a276ae3518f
Alexander96p <96xanderp@gmail.com>
2022-06-30 05:42:08 -0500

    first commit

---
+++ src/test/java/com/marlon/PruebaCI/PruebaCiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marlon.PruebaCI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaCiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
